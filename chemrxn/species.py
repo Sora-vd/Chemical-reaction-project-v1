6482f09abbbe6bdc3d746a4a6958188d844cf694
Karolina Kupiec <k.kupiec@student.maastrichtuniversity.nl>
2026-02-26 13:35:42 +0100

    changes in species, organising

--- chemrxn/species.py
+++ chemrxn/species.py
@@ -1,18 +1,17 @@
 import pandas as pd
 import os
-molecules = pd.read_csv(os.getcwd() + "/input.csv", index_col="formula")
+molecules = pd.read_csv(os.getcwd() + input.csv, index_col="formula")
 
 class Species: 
+    """A class to represent a chemical species in a reaction."""
 
-    def __init__(self, formula):
-        self.name = molecules.loc[formula, "compound"]
-        self._c0 = molecules.loc[formula, "c0"]
-        self.ct = self._c0
+    def __init__(self, name, c0, formula=None, state=None, order=0, rate_constant=None):
+        self.name = name
         self.formula = formula
-        self.state = molecules.loc[formula, "state"]
-        self.order = molecules.loc[formula, "order"]
-        self.k = None
-
+        self.state = state
+        self._c0 = c0 #use property setter to validate c0
+        
+    #initial concentration 
     @property 
     def c0(self): 
         return self._c0 
@@ -21,12 +20,18 @@ class Species:
     def c0(self, value):
         if value < 0:
             raise ValueError("Initial concentration cannot be negative")
-        self._c0 = value
-        self.ct = value
+        self._c0 = float(value)
+        self.ct = float(value) # set current concentration to initial concentration when c0 is updated
     
+    #reset concentration to initial value
     def reset(self):
         self.ct = self._c0
 
+    def update_concentration(self, new_ct):
+        if new_ct < 0:
+            raise ValueError("Concentration cannot be negative")
+        self.ct = new_ct
+    
     def __repr__(self):
         return f"Species(name={self.name}, formula={self.formula}, state={self.state}, c0={self._c0}, ct={self.ct}, order={self.order})"
     
