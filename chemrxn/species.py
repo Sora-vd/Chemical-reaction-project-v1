7af4d2ba29a33a536eb5eb0bc87b73ea1a9ee957
emma-bnm <emma.bornemann@icloud.com>
2026-02-26 13:48:33 +0100

    updated __init__ so that it reads data from file

--- chemrxn/species.py
+++ chemrxn/species.py
@@ -5,7 +5,7 @@ molecules = pd.read_csv(os.getcwd() + "/input.csv", index_col="formula")
 class Species: 
     """A class to represent a chemical species in a reaction."""
 
-    def __init__(self, name, c0, formula=None, state=None):
+    def __init__(self, formula):
         self.name = molecules.loc[formula, "compound"]
         self.formula = formula
         self.state = molecules.loc[formula, "state"]
