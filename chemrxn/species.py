78bec5fd0cf31d5b01e4c81dd16e2cbc24e0d52f
emma-bnm <emma.bornemann@icloud.com>
2026-02-26 13:58:15 +0100

    updated species.py

--- chemrxn/species.py
+++ chemrxn/species.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import os
-molecules = pd.read_csv(os.getcwd() + input.csv, index_col="formula")
+molecules = pd.read_csv(os.getcwd() + "/input.csv", index_col="formula")
 
 class Species: 
     """A class to represent a chemical species in a reaction."""
 
-    def __init__(self, name, c0, formula=None):
-        self.name = name
-        self.formula = formula
-        self._c0 = c0 #use property setter to validate c0
+    def __init__(self, formula):
+        self.name = molecules.loc[formula, "compound"]
+        self._c0 = molecules.loc[formula, "c0"] #use property setter to validate c0
+        self.formula = formula 
+        self.state = molecules.loc[formula, "state"]
         
     #initial concentration 
     @property 
@@ -32,7 +33,7 @@ class Species:
         self.ct = new_ct
     
     def __repr__(self):
-        return f"Species(name={self.name}, formula={self.formula}, state={self.state}, c0={self._c0}, ct={self.ct}, order={self.order})"
+        return f"Species(name={self.name}, formula={self.formula}, state={self.state}, c0={self._c0})"
     
 
 
